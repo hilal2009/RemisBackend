1c4f61ffe11ef813a926d2793d0d91ec0218760e
Hilali Ngereza <hngereza@gmail.com>
2023-02-15 07:45:05 +0000

    Started Building Remis Backend API Application

---
+++ src/main/java/com/whi/Remis/RemisApplication.java
@@ -0,0 +1,13 @@
+package com.whi.Remis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemisApplication.class, args);
+	}
+
+}
